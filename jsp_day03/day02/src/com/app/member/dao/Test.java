89dbd93aa29d787e9cf34d071e936a6b30d16c8d
dev-hyunjin <disappointed123419@gmail.com>
2023-02-20 18:31:46 +0900

    JSP_Day03

---
+++ jsp_day03/day02/src/com/app/member/dao/Test.java
@@ -0,0 +1,24 @@
+package com.app.member.dao;
+
+import com.app.member.domain.MemberVO;
+
+public class Test {
+	public static void main(String[] args) {
+		MemberDAO memberDAO = new MemberDAO();
+		MemberVO memberVO = new MemberVO();
+		
+//		memberDAO.selectAll().stream().map(MemberVO::getMemberName).forEach(System.out::println);
+		
+//		System.out.println(memberDAO.checkId("12342"));
+		
+//		System.out.println(memberDAO.login("hds", "1234"));
+		
+//		memberVO.setMemberName("한동석");
+//		memberVO.setMemberIdentification("hds");
+//		memberVO.setMemberPassword("1234");
+//		memberVO.setMemberBirth("2000-12-04");
+//		
+//		memberDAO.join(memberVO);
+		
+	}
+}
