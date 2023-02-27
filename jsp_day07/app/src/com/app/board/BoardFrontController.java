ca8c59c680418038d801a43a7a455272fb2af894
dev-hyunjin <disappointed123419@gmail.com>
2023-02-28 02:27:13 +0900

    JSP_Day07

---
+++ jsp_day07/app/src/com/app/board/BoardFrontController.java
@@ -0,0 +1,67 @@
+package com.app.board;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app.Result;
+
+public class BoardFrontController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		String target = uri.replace(contextPath, "").split("\\.")[0];
+		Result result = null;
+		
+		if(target.equals("/board/listOk")) {
+			result = new ListOkController().execute(req, resp);
+			
+		}else if(target.equals("/board/detailOk")){
+			result = new DetailOkController().execute(req, resp);
+			
+		}else if(target.equals("/write")){
+			
+		}else if(target.equals("/writeOk")){
+			
+		}else if(target.equals("/update")){
+			
+		}else if(target.equals("/updateOk")){
+			
+		}else {
+			
+		}
+		
+		if(result != null) {
+			if(result.isRedirect()) {
+				resp.sendRedirect(result.getPath());
+			}else {
+				req.getRequestDispatcher(result.getPath()).forward(req, resp);
+			}
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
